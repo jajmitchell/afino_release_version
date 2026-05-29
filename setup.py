78a799229f84b2e0673b8d074db6472463f55237
AIP User <aip@fondor.aip.de>
2026-05-29 05:51:38 +0200

    setup edit

--- setup.py
+++ setup.py
@@ -12,5 +12,5 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/afino_release_version/",
-    packages=setuptools.find_packages(),
+    packages=['afino'],
 )
